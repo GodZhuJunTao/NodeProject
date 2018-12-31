37e7928a18fd88a5b5a7c761de7190240d4070c3
zhujuntao <1203935376@qq.com>
2018-12-31 18:12:47 +0800

    登陆界面优化

--- src/js/register.js
+++ src/js/register.js
@@ -2,52 +2,54 @@
 
 
 //随机验证码
-    function randomNumber(min, max) {
-        return parseInt(Math.random() * (max - min + 1)) + min;
-    }
-    function randomColor(str) {
-    //生成随机颜色
-        if(str == 16) {
-            //生成16进制的 '0123456789abcdef' #666677
-            var str = '0123456789abcdef';
-            var color = '#';
-            for(var i = 0; i < 6; i++) {
-                color += str.charAt(parseInt(Math.random() * str.length));
-        }
+function randomNumber(min, max) {
+    return parseInt(Math.random() * (max - min + 1)) + min;
+}
+function randomColor(str) {
+    //生成随机颜色
+    if (str == 16) {
+        //生成16进制的 '0123456789abcdef' #666677
+        var str = '0123456789abcdef';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += str.charAt(parseInt(Math.random() * str.length));
+        }
 
-            return color;
-    
-        } else if(str == 'rgb') {
-            //rgb(255,255,0) 生成3个随机数，每个随机数应该在 0-255
-            var r = parseInt(Math.random() * 256);
-            var g = parseInt(Math.random() * 256);
-            var b = parseInt(Math.random() * 256);
-    
-            return 'rgb(' + r + ',' + g + ',' + b + ')';
-    
-        } else {
-            alert('参数传错了');
-        }
-    }
-    function randomNum(){
-    var str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    //2.生成随机思维数有字母的验证码
-    var num = str.charAt(parseInt(Math.random()*str.length));
-    return num;
-    }
-function createPicCode(){
+        return color;
+
+    } else if (str == 'rgb') {
+        //rgb(255,255,0) 生成3个随机数，每个随机数应该在 0-255
+        var r = parseInt(Math.random() * 256);
+        var g = parseInt(Math.random() * 256);
+        var b = parseInt(Math.random() * 256);
+
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+
+    } else {
+        alert('参数传错了');
+    }
+}
+function randomNum() {
+    var str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //2.生成随机思维数有字母的验证码
+    var num = str.charAt(parseInt(Math.random() * str.length));
+    return num;
+}
+function createPicCode() {
     console.log(123);
-    for(i=0;i<4;i++){
-    var html = randomNum();
-    var color = randomColor(16);
-    var foWeight = randomNumber(500,700);
-    var foSize = randomNumber(20,30);
-    $('#picCode span').eq(i).html(html).css({
-    'color':color,'fontWeight':foWeight,'fontSize':foSize
-    });
+    for (i = 0; i < 4; i++) {
+        var html = randomNum();
+        var color = randomColor(16);
+        var foWeight = randomNumber(500, 700);
+        var foSize = randomNumber(20, 30);
+        $('#picCode span').eq(i).html(html).css({
+            'color': color, 'fontWeight': foWeight, 'fontSize': foSize
+        });
     }
-    }
-        createPicCode();
-        $('#picCode').click(function(){
-            createPicCode();
-        });
+}
+createPicCode();
+$('#picCode').click(function (e) {
+    e.preventDefault();
+
+    createPicCode();
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-// 把路由封装成模块
+﻿// 把路由封装成模块
 const express = require('express');
 
 // 引入单独路由模块
@@ -9,6 +9,18 @@ const uploadRouter = require('./upload');
 const orderRouter = require('./order');
 let Router = express.Router();
 
+Router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
+    // 跨域请求CORS中的预请求
+    if(req.method=="OPTIONS") {
+      res.send(200);/*让options请求快速返回*/
+    } else{
+      next();
+    }
+});
 // 关于用户的路由
 Router.use('/login',userRouter);
 // 关于商品的路由
